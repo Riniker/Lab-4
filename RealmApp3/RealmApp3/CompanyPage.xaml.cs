0b6a91dadc0ef1a70e1662d68eae6eac9b63a7ab
Nrvnqsr <zverekqw12@gmail.com>
2020-05-24 12:43:57 +0400

    lab4

---
+++ RealmApp3/RealmApp3/CompanyPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Realms;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RealmApp3
+{
+    public partial class CompanyPage : ContentPage
+    {
+        Realm _realm;
+        Transaction _transaction;
+
+        public CompanyPage()
+        {
+            InitializeComponent();
+            _realm = Realm.GetInstance();
+            _transaction = _realm.BeginWrite();
+        }
+
+        private void SaveCompany(object sender, EventArgs e)
+        {
+            var company = (Company)BindingContext;
+            if (!String.IsNullOrEmpty(company.Name))
+            {
+                if (company.Id == null)
+                {
+                    company.Id = Guid.NewGuid().ToString();
+                    _realm.Add(company);
+                }
+
+                _transaction.Commit();
+            }
+            this.Navigation.PopAsync();
+        }
+        private void DeleteCompany(object sender, EventArgs e)
+        {
+            var company = (Company)BindingContext;
+            _realm.Remove(company);
+            _transaction.Commit();
+
+            this.Navigation.PopAsync();
+        }
+
+        protected override void OnDisappearing()
+        {
+            _transaction?.Dispose();
+            base.OnDisappearing();
+        }
+    }
+}
---
+++ RealmApp3/RealmApp3/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Realms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RealmApp3
+{
+    public partial class MainPage : ContentPage
+    {
+        private Realm _realm;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _realm = Realm.GetInstance();
+        }
+        protected override void OnAppearing()
+        {
+            companyList.ItemsSource = _realm.All<Company>();
+            base.OnAppearing();
+        }
+        // обработка нажатия элемента в списке
+        private async void TapItem(object sender, ItemTappedEventArgs e)
+        {
+            Company selectedCompany = (Company)e.Item;
+            CompanyPage companyPage = new CompanyPage
+            {
+                BindingContext = selectedCompany
+            };
+            await Navigation.PushAsync(companyPage);
+        }
+        // обработка нажатия кнопки добавления
+        private async void CreateCompany(object sender, EventArgs e)
+        {
+            CompanyPage companyPage = new CompanyPage
+            {
+                BindingContext = new Company()
+            };
+            await Navigation.PushAsync(companyPage);
+        }
+    }
+}
